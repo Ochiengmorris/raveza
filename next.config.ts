ae52301bed847cb0170270a255a39a1565c7c04a
OchiengMorris <oduyajohn66@gmail.com>
2025-04-30 21:18:23 +0300

    fix-images-hostname

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { hostname: "fabulous-iguana-786.convex.cloud", protocol: "https" },
+      { hostname: "beaming-kingfisher-974.convex.cloud", protocol: "https" },
       { hostname: "img.clerk.com", protocol: "https" },
     ],
   },
